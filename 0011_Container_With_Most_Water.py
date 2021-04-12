c72c55961ac00e5b69dd406d93774c276ddd3b57
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-12 09:34:30 +0300

    Create 0011_Container_With_Most_Water.py
    Runtime: 760 ms, faster than 17.67% of Python3 online submissions for Container With Most Water.
    Memory Usage: 26.5 MB, less than 38.12% of Python3 online submissions for Container With Most Water.

---
+++ 0011_Container_With_Most_Water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maximum = 0
+        i = 0
+        j = len(height) - 1
+        
+        while i < j:
+            minimum = min(height[i], height[j])
+            maximum = max(maximum, minimum * (j - i))
+            
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+                
+        return maximum
