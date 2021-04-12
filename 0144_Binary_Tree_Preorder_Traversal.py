abb1c04d76bf2989fe0fd5e3d4192fd4a78f6021
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-12 14:42:19 +0300

    Create 0144_Binary_Tree_Preorder_Traversal.py
    Runtime: 32 ms, faster than 53.34% of Python3 online submissions for Binary Tree Preorder Traversal.
    Memory Usage: 14.1 MB, less than 90.96% of Python3 online submissions for Binary Tree Preorder Traversal.

---
+++ 0144_Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        arr = []
+        self.traverse(root, arr)
+        return arr
+    
+    def traverse(self, root, arr):
+        if root:
+            arr.append(root.val)
+            self.traverse(root.left, arr)
+            self.traverse(root.right, arr)
+            
