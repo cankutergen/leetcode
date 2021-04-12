8fd4ff80a3ea5f3ee0f823de2e76272060478ea2
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-12 15:16:12 +0300

    Create 0107_Binary_Tree_Level_Order_Traversal_II.py
    Runtime: 44 ms, faster than 5.31% of Python3 online submissions for Binary Tree Level Order Traversal II.
    Memory Usage: 14.6 MB, less than 47.06% of Python3 online submissions for Binary Tree Level Order Traversal II.

---
+++ 0107_Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+import queue
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        arr = []
+        q = queue.Queue()
+        
+        if root:
+            q.put(root)
+        
+        while not q.empty():
+            level = []
+            
+            for _ in range(q.qsize()):             
+                node = q.get()
+                level.append(node.val)
+
+                if node.left is not None:
+                    q.put(node.left)
+
+                if node.right is not None:
+                    q.put(node.right)
+                    
+            arr.append(level)
+                
+        return reversed(arr)
