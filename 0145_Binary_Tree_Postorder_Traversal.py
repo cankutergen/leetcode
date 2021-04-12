248bc5c0d3397475b20e61d96972f9c3d7cf6ade
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-12 14:54:13 +0300

    Create 0145_Binary_Tree_Postorder_Traversal.py
    Runtime: 32 ms, faster than 54.00% of Python3 online submissions for Binary Tree Postorder Traversal.
    Memory Usage: 14.3 MB, less than 13.72% of Python3 online submissions for Binary Tree Postorder Traversal.

---
+++ 0145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        arr = []
+        self.traverse(root, arr)
+        return arr
+        
+    def traverse(self, root, arr):
+        if root:
+            self.traverse(root.left, arr)
+            self.traverse(root.right, arr)
+            arr.append(root.val)
