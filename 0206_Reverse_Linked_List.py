b74fa088fa28c4cfda12de5d0ac6ccf991ff5f8d
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-12 22:36:42 +0300

    Update 0206_Reverse_Linked_List.py
    Runtime: 32 ms, faster than 85.57% of Python3 online submissions for Reverse Linked List.
    Memory Usage: 15.5 MB, less than 92.61% of Python3 online submissions for Reverse Linked List.

--- 0206_Reverse_Linked_List.py
+++ 0206_Reverse_Linked_List.py
@@ -5,18 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        stack = []
-        
-        while head is not None:
-            stack.append(head)
-            head = head.next
-            
-        dummy = ListNode(-1)
-        head = dummy
-        
-        while len(stack) is not 0:
-            current = stack.pop()
-            head.next = ListNode(current.val)
-            head = head.next
-            
-        return dummy.next
+        dummy_head = None
+
+        while head:
+            next_node = head.next
+            head.next = dummy_head
+            dummy_head = head
+            head = next_node
+
+        return dummy_head
