62a8a92ed033546769b04685b2d62a54bead85c1
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-12 09:32:17 +0300

    Create 0007_Reverse_Integer.py
    Runtime: 32 ms, faster than 65.09% of Python3 online submissions for Reverse Integer.
    Memory Usage: 14.1 MB, less than 90.72% of Python3 online submissions for Reverse Integer.

---
+++ 0007_Reverse_Integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        negative = False
+        if x < 0:
+            x *= -1
+            negative = True
+
+        number = str(x)
+
+        num = 0
+        for i in range(len(number)):
+            num += (10 ** i) * int(number[i])
+
+        if num > 2**31 - 1:
+            return 0
+        
+        if negative:
+            num *= -1
+
+        return num
