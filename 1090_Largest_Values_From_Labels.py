0112d9682621bd43dc36532b09306bd3f5bd336e
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-05-07 11:01:06 +0300

    Update 1090_Largest_Values_From_Labels.py
    Runtime: 140 ms, faster than 48.00% of Python3 online submissions for Largest Values From Labels.
    Memory Usage: 19 MB, less than 72.57% of Python3 online submissions for Largest Values From Labels.

--- 1090_Largest_Values_From_Labels.py
+++ 1090_Largest_Values_From_Labels.py
@@ -1,31 +1,27 @@
-class Item:
-    def __init__(self, value, label):
-        self.value = value
-        self.label = label
+from heapq import heappop, heappush, heapify
 
+        # N logn(N)
 class Solution:
     def largestValsFromLabels(self, values: List[int], labels: List[int], num_wanted: int, use_limit: int) -> int:
-        items = []
-        item_dict = {}
+        max_heap = []
+        heapify(max_heap)
         
         for i in range(len(labels)):
-            items.append(Item(values[i], labels[i]))
-            item_dict[labels[i]] = 0
-            
-        items.sort(key = lambda x: x.value, reverse = True)
+            heappush(max_heap, (-values[i], labels[i]))
 
-        numbers = 0
-        total = 0
-        for i in range(len(values)):
-            val = items[i].value
-            used_times = item_dict[items[i].label]
+        counts = {}
+        result = 0
+        
+        while max_heap and num_wanted > 0:
+            val, label = heappop(max_heap)
+            
+            if label not in counts:
+                counts[label] = 1
+            else:
+                counts[label] += 1
+            
+            if counts[label] <= use_limit:
+                result += -val
+                num_wanted -= 1
                 
-            if used_times < use_limit:
-                total += val
-                item_dict[items[i].label] = used_times + 1
-                numbers += 1
-                
-            if numbers >= num_wanted:
-                break
-                
-        return total
+        return result
